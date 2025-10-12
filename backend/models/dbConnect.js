093d28bcc83ebdeb9f1c3dfeaddd67d2ec901444
BHUSHN1611 <bhushanjadhav1603@gmail.com>
2025-10-12 19:39:32 +0530

    fix:DB error fixed

--- backend/models/dbConnect.js
+++ backend/models/dbConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const DB = process.env.DB_URL;
+const DB = "mongodb+srv://bhushn:railmadat16@cluster0.aeoisat.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 console.log('DB--', DB);
 mongoose
     .connect(DB)
